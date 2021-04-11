2125ffbb13fa1383f5a67c183179a1bef3382488
Emerson Gaudencio <emerson.fender@gmail.com>
2021-04-11 20:38:27 +0100

    updated backend gcs bucket

--- deploy_mysql_source_replica/00_configure-remote-state.sh
+++ deploy_mysql_source_replica/00_configure-remote-state.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+# https://www.terraform.io/docs/language/settings/backends/gcs.html
 # backend remote state
 echo 'terraform {
   backend "gcs" {
     bucket      = "terraform-state-turbo-dba-dev-gcp"
-    prefix      = "database-tfstate"
+    prefix      = "mysql-source-replica/terraform.tfstate"
     credentials = "CREDENTIALS_FILE.json"
   }
 }
